f67bf30502dcc9557f021ded9484feeff0e3ac03
Kyu-u <melanchthonbutarbutar@gmail.com>
2021-08-17 22:52:45 +0700

    skooooooooooooooooo

---
+++ resources/views/admin2.blade.php
@@ -0,0 +1,134 @@
+@extends('layouts.adminnav ')
+@section('content')
+<div class="flex flex-col items-center">
+    <h1 class="text-4xl sm:text-6xl pb-20 text-center " style="background: #57CA85;
+    background: -webkit-linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+    background: -moz-linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+    background: linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;">Administration</h1>
+    <div class="home text relative flex justify-center mx-auto w-6/7 h-96 sm:h-97 sm:w-97  md:w-98 lg:w-99">
+
+        <div class="relative w-full h-full flex-1 rounded-3xl bg-gradient-to-r from-signup-0 to-tableisi-0 p-10">
+
+
+            <form action="{{route('regis2')}}" method="POST" class="pt-2 sm:pt-5 flex flex-row ">
+                @csrf
+                <div class="flex flex-col items-start w-3/7">
+
+
+                    <div class="mt-5 flex flex-col text-xl gap-7 overflow-y-scroll items-center h-64 sm:h-72 md:h-80 lg:h-80  scrollbar-thumb-rounded-full scrollbar-thin object-contain
+                  scrollbar-track-main-0 scrollbar-thumb-signup-0 pr-10">
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                        <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+
+                    </div>
+                </div>
+                <div class="flex flex-col w-4/7">
+                    <h2 class="text-white font-sans text-sm sm:text-2xl font-semibold">
+                        Please fill out the form.
+                    </h2>
+                    <label class="pb-2 text-sm:text-xl text-white" for="Email/Phone">Name</label>
+                    <input class="text-black outline-none px-3 w-full lg:w-1/2 rounded-md text-sm py-1 sm:py-4 "
+                        type="text" id="name" name="name" placeholder="Your Name...">
+                    <label class="pt-2 pb-2 text-sm:text-xl text-white" for="Email/Phone">Country</label>
+                    <input class="text-black outline-none px-3 w-full lg:w-1/2 rounded-md text-sm py-1 sm:py-4 "
+                        type="text" id="country" name="country" placeholder="Your Country...">
+                    <label class="pt-2 pb-2 text-sm:text-xl text-white" for="Email/Phone">Council</label>
+                    <input class="text-black outline-none px-3 w-full lg:w-1/2 rounded-md text-sm py-1 sm:py-4 "
+                        type="text" id="council" name="council" placeholder="Your Council...">
+
+
+
+                    <a class="self-end pt-2 sm:pt-5" href={{route('signup3')}}>
+                        <button type="submit"
+                            class="bg-signup-0 text-white text-sm sm:text-xl font-semibold font-sans w-20 sm:w-40 h-10 sm:h-14 rounded-md self-center mt-2 ml-20 flex justify-center items-center">
+                            Submit
+                        </button>
+                    </a>
+                </div>
+
+
+            </form>
+
+
+        </div>
+
+
+    </div>
+
+    <div class="relative flex justify-center mx-auto w-6/7 h-96 sm:h-97 sm:w-97  md:w-98 lg:w-99">
+
+        <div class="relative w-full h-full flex-1 rounded-3xl bg-gradient-to-r from-signup-0 to-tableisi-0 p-10 text-white">
+
+
+          <form action="{{route('regis2')}}" method="POST" class="pt-2 sm:pt-5 flex flex-row ">
+            @csrf
+            <div class="flex flex-col items-start w-3/7">
+
+
+              <div class="mt-5 flex flex-col text-xl gap-7 overflow-y-scroll items-center h-64 sm:h-72 md:h-80 lg:h-80  scrollbar-thumb-rounded-full scrollbar-thin object-contain
+              scrollbar-track-main-0 scrollbar-thumb-signup-0 pr-10">
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+                <div class="flex-wrap"> Drigo Alexander Sihombing</div>
+
+              </div>
+            </div>
+            <div class="flex flex-col w-4/7">
+              <h2 class="text-white font-sans text-sm sm:text-2xl font-semibold">
+                Please fill out the form.
+              </h2>
+              <label class="pb-2 text-sm:text-xl text-white" for="Email/Phone">Name</label>
+              <input class="text-black outline-none px-3 w-full lg:w-1/2 rounded-md text-sm py-1 sm:py-4 " type="text"
+                id="name" name="name" placeholder="Your Name...">
+              <label class="pt-2 pb-2 text-sm:text-xl text-white" for="Email/Phone">Country</label>
+              <input class="text-black outline-none px-3 w-full lg:w-1/2 rounded-md text-sm py-1 sm:py-4 " type="text"
+                id="country" name="country" placeholder="Your Country...">
+              <label class="pt-2 pb-2 text-sm:text-xl text-white" for="Email/Phone">Council</label>
+              <input class="text-black outline-none px-3 w-full lg:w-1/2 rounded-md text-sm py-1 sm:py-4 " type="text"
+                id="council" name="council" placeholder="Your Council...">
+
+
+
+              <a class="self-end pt-2 sm:pt-5" href={{route('signup3')}}>
+                <button type="submit"
+                  class="bg-signup-0 text-white text-sm sm:text-xl font-semibold font-sans w-20 sm:w-40 h-10 sm:h-14 rounded-md self-center mt-2 ml-20 flex justify-center items-center">
+                  Submit
+                </button>
+              </a>
+            </div>
+
+
+          </form>
+
+
+        </div>
+
+
+      </div>
+</div>
+@endsection
