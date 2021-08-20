70d0a41c5ced5122397cd2b71d1b906c3d8ec5d8
Drigo Alexander <drigosihombinga@gmail.com>
2021-08-20 13:17:48 +0700

    add prevent user from zooming in mobile

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1,viewport-fit=cover,minimum-scale=1,maximum-scale=1,user-scalable=no">
   <link rel="stylesheet" href="/css/app.css">
   <title>Surabaya Model United Nations</title>
   <meta name="description"
