e0ec562590ce3004d48f8a44457f01dc881a851c
Drigo Alexander <drigosihombinga@gmail.com>
2021-08-28 22:02:05 +0700

    merchandise page

---
+++ resources/views/merch.blade.php
@@ -0,0 +1,200 @@
+@extends('layouts.navbarnotlogin')
+@section('content')
+
+
+<section class="bg-main-0 py-20 flex justify-center items-center relative">
+        <img src="images/ornamenmerch1.png" alt="" class="absolute top-0 left-0 z-0 w-1/2 md:w-1/3 animate-pulse">
+        <img src="images/ornamenmerch2.png" alt="" class="absolute bottom-0 right-0 z-0 w-1/2 md:w-1/6 animate-pulse ">
+        <div class="mx-auto max-w-7xl z-10 relative ">
+            <div class="flex flex-col justify-center items-center">
+                <h1 class="text-5xl" style="background: #57CA85;
+                background: -webkit-linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+                background: -moz-linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+                background: linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;">
+                    Merchandise
+                </h1>
+                <!-- START : CARD MERCH -->
+                <div class="py-32 flex flex-col md:flex-row">
+                    <!-- START : INSIDE CARD MERCH -->
+                    <div class="px-0 md:px-4 py-4 md:py-0" data-aos="fade-left" data-aos-duration="1500">
+                        <div
+                            class="w-auto transform scale-100 md:hover:scale-110 duration-300 bg-gradient-to-r from-signup-0 to-timelinebot-0 rounded-xl">
+                            <div class="flex flex-row md:flex-col justify-center items-center">
+                                <!-- START : HEADER CARD -->
+                                <div class="-ml-4 md:-ml-0 -mt-0 md:-mt-16 w-36 md:w-40 rounded-full overflow-hidden">
+                                    <img src="images/merchToteBag.png" alt="Merchandise Tote Bag Surabaya Model United Nations">
+                                </div>
+                                <!-- END : HEADER CARD -->
+                                <!-- START : WRAPPER PADDING -->
+                                <div class="px-4 md:px-16 py-2 md:py-6">
+                                    <!-- START : BODY CARD -->
+                                    <div class="py-3 md:py-6 flex flex-col justify-center items-center">
+                                        <h1 class="text-md md:text-lg text-signup-0 self-center">
+                                            <div class="font-semibold">
+                                                Tote Bag
+                                            </div>
+
+                                            <div class="flex justify-center items-center pt-1">
+                                                <div class="w-1/2 h-0.5 bg-signup-0 self-center rounded-xl">
+                                                </div>
+                                            </div>
+                                        </h1>
+
+                                        <div class="text-sm md:text-md text-white font-semibold pt-4 md:pt-6">
+                                            Rp 60.000,-
+                                        </div>
+
+
+                                        <div class="text-xs md:text-sm text-white font-thin pt-4 md:pt-6">
+                                            <ul class="flex flex-col justify-center items-center">
+                                                
+                                                <li>Ukuran : 19cm x 20cm</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <!-- END : BODY CARD -->
+
+                                    <!-- START : BUTTON BUY -->
+                                    <div class="py-3 md:py-6 text-white flex justify-center items-center">
+                                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeR7mtNkxbXry-Lw51255YfNM6NGHwNihn14NgrAgtp3_VQ4Q/viewform" target="_blank"
+                                            class="w-auto bg-signup-0 active:bg-buttonmerch-0 transition ease-out duration-200 px-4 md:px-6 py-1 rounded-md text-xs md:text-sm">
+                                            Buy
+                                        </a>
+
+                                    </div>
+                                    <!-- END : BUTTON BUY -->
+                                </div>
+                                <!-- END : WRAPPER PADDING -->
+                            </div>
+                        </div>
+                        <!-- END : INSIDE CARD MERCH -->
+                    </div>
+
+
+                    <div class="px-0 md:px-4 py-4 md:py-0" data-aos="fade-up" data-aos-duration="1500">
+                        <div
+                            class="w-auto transform scale-100 md:hover:scale-110 duration-300 bg-gradient-to-r from-signup-0 to-timelinebot-0 rounded-xl">
+                            <div class="flex flex-row md:flex-col justify-center items-center">
+                                <!-- START : HEADER CARD -->
+                                <div class="-ml-4 md:-ml-0 -mt-0 md:-mt-16 w-36 md:w-40 rounded-full overflow-hidden">
+                                    <img src="images/merchT-Shirt.png" alt="Merchandise T-Shirt Surabaya Model United Nations">
+                                </div>
+                                <!-- END : HEADER CARD -->
+                                <!-- START : WRAPPER PADDING -->
+                                <div class="px-4 md:px-16 py-2 md:py-6">
+                                    <!-- START : BODY CARD -->
+                                    <div class="py-3 md:py-6 flex flex-col justify-center items-center">
+                                        <h1 class="text-md md:text-lg text-signup-0 self-center">
+                                            <div class="font-semibold">
+                                                T-shirt 
+                                            </div>
+
+                                            <div class="flex justify-center items-center pt-1">
+                                                <div class="w-1/2 h-0.5 bg-signup-0 self-center rounded-xl">
+                                                </div>
+                                            </div>
+                                        </h1>
+
+                                        <div class="text-sm md:text-md text-white font-semibold pt-4 md:pt-6">
+                                            Rp 90.000,-
+                                        </div>
+
+
+                                        <div class="text-xs md:text-sm text-white font-thin pt-4 md:pt-6">
+                                            <ul class="flex flex-col justify-center items-center">
+                                                
+                                                <li>Size : On Google Form</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <!-- END : BODY CARD -->
+
+                                    <!-- START : BUTTON BUY -->
+                                    <div class="py-3 md:py-6 text-white flex justify-center items-center">
+                                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeR7mtNkxbXry-Lw51255YfNM6NGHwNihn14NgrAgtp3_VQ4Q/viewform" target="_blank"
+                                            class="w-auto bg-signup-0 active:bg-buttonmerch-0 transition ease-out duration-200 px-4 md:px-6 py-1 rounded-md text-xs md:text-sm">
+                                            Buy
+                                        </a>
+
+                                    </div>
+                                    <!-- END : BUTTON BUY -->
+                                </div>
+                                <!-- END : WRAPPER PADDING -->
+                            </div>
+                        </div>
+                        <!-- END : INSIDE CARD MERCH -->
+                    </div>
+
+
+                    <div class="px-0 md:px-4 py-4 md:py-0" data-aos="fade-right" data-aos-duration="1500">
+                        <div
+                            class="w-auto transform scale-100 md:hover:scale-110 duration-300 bg-gradient-to-r from-signup-0 to-timelinebot-0 rounded-xl">
+                            <div class="flex flex-row md:flex-col justify-center items-center">
+                                <!-- START : HEADER CARD -->
+                                <div class="-ml-4 md:-ml-0 -mt-0 md:-mt-16 w-36 md:w-40 rounded-full overflow-hidden">
+                                    <img src="images/merchSticker.png" alt="Merchandise Sticker Pack Surabaya Model United Nations">
+                                </div>
+                                <!-- END : HEADER CARD -->
+                                <!-- START : WRAPPER PADDING -->
+                                <div class="px-4 md:px-16 py-2 md:py-6">
+                                    <!-- START : BODY CARD -->
+                                    <div class="py-3 md:py-6 flex flex-col justify-center items-center">
+                                        <h1 class="text-md md:text-lg text-signup-0 self-center">
+                                            <div class="font-semibold">
+                                                Sticker
+                                            </div>
+
+                                            <div class="flex justify-center items-center pt-1">
+                                                <div class="w-1/2 h-0.5 bg-signup-0 self-center rounded-xl">
+                                                </div>
+                                            </div>
+                                        </h1>
+
+                                        <div class="text-sm md:text-md text-white font-semibold pt-4 md:pt-6">
+                                            Rp 20.000,-
+                                        </div>
+
+
+                                        <div class="text-xs md:text-sm text-white font-thin pt-4 md:pt-6">
+                                            <ul class="flex flex-col justify-center items-center">
+                                                
+                                                <li>Ukuran : 19cm x 20cm</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <!-- END : BODY CARD -->
+
+                                    <!-- START : BUTTON BUY -->
+                                    <div class="py-3 md:py-6 text-white flex justify-center items-center">
+                                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeR7mtNkxbXry-Lw51255YfNM6NGHwNihn14NgrAgtp3_VQ4Q/viewform" target="_blank"
+                                            class="w-auto bg-signup-0 active:bg-buttonmerch-0 transition ease-out duration-200 px-4 md:px-6 py-1 rounded-md text-xs md:text-sm">
+                                            Buy
+                                        </a>
+
+                                    </div>
+                                    <!-- END : BUTTON BUY -->
+                                </div>
+                                <!-- END : WRAPPER PADDING -->
+                            </div>
+                        </div>
+                        <!-- END : INSIDE CARD MERCH -->
+                    </div>
+
+
+
+
+
+
+
+
+
+                    <!-- END : CARD MERCH -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+@endsection
