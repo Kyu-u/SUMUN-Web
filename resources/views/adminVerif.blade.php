19ef687afc7baa74ecce5afcb59f7b9cf3b06def
Kyu-u <melanchthonbutarbutar@gmail.com>
2021-08-29 15:26:45 +0700

    Update adminVerif.blade.php

--- resources/views/adminVerif.blade.php
+++ resources/views/adminVerif.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.adminnav ')
 @section('content')
-<section class="bg-main-0 flex justify-center">
+<section class="bg-main-0 flex flex-col max-w-7xl mx-auto">
+    <h1 class="text-5xl text-center pb-10" style="background: #57CA85;
+    background: -webkit-linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+    background: -moz-linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+    background: linear-gradient(to bottom, #57CA85 0%, #184E68 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;">Verification</h1>
     <div class="flex flex-wrap justify-center items-center px-10">
         @foreach($images as $image)
-        <div class="w-auto bg-signup-0 px-16 py-6 rounded-xl flex flex-col">
-            <form method="post" action="{{route('verify.post')}}">
+        <form method="post" action="{{route('verify.post')}}">
+
+            <div
+                class="w-97 bg-gradient-to-r from-signup-0 to-tableisi-0 px-16 gap-y-8 py-6 rounded-xl flex flex-col items-center text-white">
                 @csrf
-                <p>
+                <p class="text-2xl">
                     @if($image->type=='MUN')
                     Pembayaran MUN
                     @else
                     Pembayaran Webinar
                     @endif
                 </p>
-                <div class="flex flex-row">
-                    <div class="w-32 h-44 overflow-hidden bg-gray-100">
+                <div class="flex flex-row w-full gap-x-10 justify-between">
+                    <div class="w-32 h-44 overflow-hidden flex items-center">
                         <img src="{{ route('image.displayImage',$image->file_path) }}" alt="" />
                     </div>
 
-                    <div class="flex flex-col pl-6 h-44 overflow-y-scroll pl-6">
+                    <div class="flex flex-col flex-1 items-start h-44 overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-thin object-contain
+                    scrollbar-track-main-0 scrollbar-thumb-signup-0 pl-6">
                         @foreach($users as $user)
                         @if($image->user_id == $user->user_id)
                         <p class="py-2">
@@ -30,14 +39,15 @@
                     </div>
                 </div>
                 <input type="hidden" id="user_id" name="user_id" value="{{$image->user_id}}">
-                <button type="submit" class="w-auto px-4 py-2 bg-signup-0">
+                <button type="submit" class="w-auto rounded-md px-4 py-2 bg-signup-0">
 
-                    submit
+                    Verify
 
                 </button>
-            </form>
 
-        </div>
+            </div>
+        </form>
+
         @endforeach
     </div>
 </section>
