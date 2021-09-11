bce3127262432aceb74e65fd0fdf2c00df2c0500
drigoalexander <73428164+drigoalexander@users.noreply.github.com>
2021-09-11 23:22:32 +0700

    update verifEmail

--- resources/views/verificationEmail.blade.php
+++ resources/views/verificationEmail.blade.php
@@ -1,4 +1,62 @@
-<h1>Email Verification Mail</h1>
+<body class="body"
+	style="padding:0 !important; margin:0 !important; display:block !important; min-width:100% !important; width:100% !important; background:#469766; -webkit-text-size-adjust:none;">
+	<table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#469766">
+		<tr>
+			<td align="center" valign="top">
+				<table width="650" border="0" cellspacing="0" cellpadding="0" class="mobile-shell">
+					<tr>
+						<td class="td container"
+							style="width:650px; min-width:650px; font-size:0pt; line-height:0pt; margin:0; font-weight:normal; padding:180px 0px; ">
+							<!-- Header -->
+							<table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin: auto;">
+								<tr>
+									<td class="p30-15" style="padding: 0px 30px 30px 30px;">
+										<table width="100%" border="0" cellspacing="0" cellpadding="0">
+											<tr>
+												<th class="column-empty" width="1"
+													style="font-size:0pt; line-height:0pt; padding:0; margin:0; font-weight:normal; vertical-align:top;">
+												</th>
+												
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table>
+							<!-- END Header -->
 
-Please verify your email with bellow link:
-<a href="{{ route('user.verify', $token) }}">Verify Email</a>
+							<!-- Intro -->
+							<table width="100%" border="0" cellspacing="0" cellpadding="0">
+								<tr>
+									<td style="padding-bottom: 10px; justify-content: center; display:flex; align-items: center;">
+										<table width="75%" border="0" cellspacing="0" cellpadding="0">
+											<tr>
+												<td class="tbrr p30-15"
+													style="padding: 60px 30px;  background: #57CA85;"
+													>
+													<table width="100%" border="0" cellspacing="0" cellpadding="0">
+														<tr style="display:flex; flex-direction:column;">
+															<td class="h1 pb25"
+																style="color:#ffffff; font-family:'Muli', Arial,sans-serif; font-size:30px; line-height:36px; text-align:left; ">
+																Verify your email
+																</td>
+															<td style="color:#ffffff; font-family:'Muli', Arial,sans-serif; font-size:12px; line-height:22px; text-align:left; padding:10px 0 10px 0; padding-right: 20px;">
+																Click the link bellow to verify your email and start your journey in Surabaya MUN
+															</td>
+															<td style="padding:10px 0; width: 50%; margin:auto; background-color: #469766; display:flex; justify-content:center; font-family:'Muli', Arial,sans-serif; font-size:14px; line-height:22px; text-align:left; padding:10px 0; ">
+																<a href="{{ route('user.verify', $token) }}" style="color:#ffffff;">Verify Email</a>
+															</td>
+															
+														</tr>
+														
+														<!-- END Button -->
+													</table>
+												</td>
+											</tr>
+										</table>
+									</td>
+								</tr>
+							</table>
+							<!-- END Intro -->
+
+							
+</body>
