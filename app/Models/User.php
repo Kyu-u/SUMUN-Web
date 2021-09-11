dbb811d1f87fcb50d67f45a5a8ede38a97671c08
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-09-11 22:49:52 +0700

    zuzu

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable 
 {
     use HasFactory;
     use Notifiable;
@@ -38,6 +38,7 @@ class User extends Authenticatable
         'mun_id',
         'verified',
         'transaction_id',
+        'is_email_verified',
         'created_at',
         'updated_at',
     ];
